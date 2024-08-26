3393dcfcfd0f738984e98efcae0695b8e3f40285
Adrian.Juszczuk <108907406+Rogrum@users.noreply.github.com>
2024-08-26 21:53:22 +0200

    Add files via upload

--- SauceDemoAutomation.Tests.cs
+++ SauceDemoAutomation.Tests.cs
@@ -1,17 +1,14 @@
-﻿using log4net;
-using log4net.Config;
+﻿using Serilog;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using SauceDemoAutomation.Pages;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.IO;
 
 namespace SauceDemoAutomation.Tests
 {
     [TestClass]
     public class LoginTests
     {
-        private static readonly ILog Logger = LogManager.GetLogger(typeof(LoginTests));
+        private static ILogger Logger;
 
         [ThreadStatic]
         private static IWebDriver? Driver;
@@ -19,24 +16,40 @@ namespace SauceDemoAutomation.Tests
 
         private const string PageUrl = "https://www.saucedemo.com/";
 
+        [ClassInitialize]
+        public static void GlobalSetup(TestContext context)
+        {
+            Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/test.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Logger.Information("Serilog configured successfully.");
+        }
+
         [TestInitialize]
         public void Setup()
         {
-            XmlConfigurator.Configure(new FileInfo("log4net.config"));
-            Logger.Info("Starting test setup");
+            Logger.Information("Starting test setup");
+
+            if (!Directory.Exists("logs"))
+            {
+                Directory.CreateDirectory("logs");
+            }
 
             Driver = new ChromeDriver();
             Driver.Navigate().GoToUrl(PageUrl);
             LoginPage = new LoginPage(Driver);
 
-            Logger.Info("Browser opened and navigated to the page.");
+            Logger.Information("Browser opened and navigated to the page.");
         }
 
         [DataTestMethod]
         [DataRow("", "", "Epic sadface: Username is required")]
         public void UC1_EmptyCredentials_ShowsUsernameRequired(string username, string password, string expectedError)
         {
-            Logger.Info("Test UC1_EmptyCredentials_ShowsUsernameRequired started");
+            Logger.Information("Test UC1_EmptyCredentials_ShowsUsernameRequired started");
 
             LoginPage!.EnterUsername(username);
             LoginPage.EnterPassword(password);
@@ -47,14 +60,14 @@ namespace SauceDemoAutomation.Tests
 
             Assert.IsTrue(errorMessage.Contains(expectedError), $"Expected '{expectedError}' but found '{errorMessage}'.");
 
-            Logger.Info("Test UC1_EmptyCredentials_ShowsUsernameRequired finished");
+            Logger.Information("Test UC1_EmptyCredentials_ShowsUsernameRequired finished");
         }
 
         [DataTestMethod]
         [DataRow("standard_user", "", "Epic sadface: Password is required")]
         public void UC2_MissingPassword_ShowsPasswordRequired(string username, string password, string expectedError)
         {
-            Logger.Info("Test UC2_MissingPassword_ShowsPasswordRequired started");
+            Logger.Information("Test UC2_MissingPassword_ShowsPasswordRequired started");
 
             LoginPage!.EnterUsername(username);
             LoginPage.EnterPassword(password);
@@ -65,14 +78,14 @@ namespace SauceDemoAutomation.Tests
 
             Assert.IsTrue(errorMessage.Contains(expectedError), $"Expected '{expectedError}' but found '{errorMessage}'.");
 
-            Logger.Info("Test UC2_MissingPassword_ShowsPasswordRequired finished");
+            Logger.Information("Test UC2_MissingPassword_ShowsPasswordRequired finished");
         }
 
         [DataTestMethod]
         [DataRow("standard_user", "secret_sauce")]
         public void UC3_ValidCredentials_NavigatesToDashboard(string username, string password)
         {
-            Logger.Info("Test UC3_ValidCredentials_NavigatesToDashboard started");
+            Logger.Information("Test UC3_ValidCredentials_NavigatesToDashboard started");
 
             LoginPage!.EnterUsername(username);
             LoginPage.EnterPassword(password);
@@ -80,20 +93,28 @@ namespace SauceDemoAutomation.Tests
 
             Assert.IsTrue(Driver!.Title.Contains("Swag Labs"));
 
-            Logger.Info("Test UC3_ValidCredentials_NavigatesToDashboard finished");
+            Logger.Information("Test UC3_ValidCredentials_NavigatesToDashboard finished");
         }
 
         [TestCleanup]
         public void Teardown()
         {
-            Logger.Info("Test teardown started");
+            Logger.Information("Test teardown started");
             if (Driver != null)
             {
                 Driver.Quit();
                 Driver.Dispose();
-                Logger.Info("Browser closed and disposed.");
+                Logger.Information("Browser closed and disposed.");
             }
+        }
+
+        [ClassCleanup]
+        public static void GlobalTeardown()
+        {
+            Logger.Information("Test class teardown completed.");
+            Log.CloseAndFlush();
         }
     }
 }
+
 
